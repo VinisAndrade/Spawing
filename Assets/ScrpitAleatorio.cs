dd14182f759439d9387a478edff7bc0d8dfaf7c2
quejo18 <36921852+quejo18@users.noreply.github.com>
2022-01-20 23:47:16 -0300

    Spawing
    Project spawing object.

---
+++ Assets/ScrpitAleatorio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrpitAleatorio : MonoBehaviour
+{
+    public GameObject objetoSpawn;
+    private int xPos;
+    private int zPos;
+    private int yPos;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnDrop());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnDrop(){
+        while(count < 100){
+            xPos = Random.Range(1,50); //Mínimo e máximo de X e Y
+            yPos = Random.Range(0,10);
+            zPos = Random.Range(1,50);
+
+            Instantiate(objetoSpawn, new Vector3(xPos, yPos, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            count++;
+        }
+    }
+}
---
+++ Assets/Spawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+    public GameObject objectSpawn;
+    public int xPos;
+    public int zPos;
+    public int spawnCount;
+
+    void Start () {
+        StartCoroutine(EnemyDrop()); //Uma corrotina é utilizada para modelar comportamentos que irão impactar inúmeros quadros simultâneamente.
+    }
+
+    IEnumerator EnemyDrop () {
+        while (spawnCount < 10) {
+            xPos = Random.Range(1, 50); //Minimo e máximo
+            zPos = Random.Range (1, 50);
+            Instantiate(objectSpawn, new Vector3 (xPos, 0.5f, zPos), Quaternion.identity); 
+            //Instantiate criar a instância de um novo objeto
+            //Quaternion processa a rotação aleatória dos objetos para que possam ser apresentados em cena
+            yield return new WaitForSeconds(0.9f);
+
+            spawnCount++;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
